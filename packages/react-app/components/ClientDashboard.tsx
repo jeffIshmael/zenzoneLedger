96c305a395b1530649f80da0cfbc24c79e0ca3b1
jeffIshmael <jeffishmael141@gmail.com>
2024-07-10 22:35:30 +0300

    correcting dashboard

--- packages/react-app/components/ClientDashboard.tsx
+++ packages/react-app/components/ClientDashboard.tsx
@@ -467,11 +467,10 @@ const ClientDashboard = () => {
                 </div>
                 <div className="p-2 md:block text-left">
                   <h2 className="text-sm font-semibold text-gray-800">
-                    {details?.username}
+                    {details.username}
                   </h2>
                   <p className="text-xs text-gray-500">
-                    {details?.walletAddress.slice(0, 6)}...
-                    {details?.walletAddress.slice(-3)}
+                    {details.walletAddress?.slice(0,6)}...{details.walletAddress?.slice(-3)}
                   </p>
                 </div>
               </button>
@@ -595,7 +594,7 @@ const ClientDashboard = () => {
               <div className="flex justify-between mb-4">
                 <div>
                   <div className="flex items-center mb-1">
-                    <div className="text-2xl font-semibold">{details?.purchasedIds.length}</div>
+                    <div className="text-2xl font-semibold">{details.purchasedIds?.length}</div>
                     <div className="p-1 rounded bg-emerald-500/10 text-emerald-500 text-[12px] font-semibold leading-none ml-2"></div>
                   </div>
                   <div className="text-sm font-medium text-gray-400">
@@ -645,7 +644,7 @@ const ClientDashboard = () => {
             <div className="bg-white rounded-md border border-gray-100 p-6 shadow-md shadow-black/5">
               <div className="flex justify-between mb-6">
                 <div>
-                  <div className="text-2xl font-semibold mb-1">{details?.purchasedIds.length}</div>
+                  <div className="text-2xl font-semibold mb-1">{details.purchasedIds?.length}</div>
                   <div className="text-sm font-medium text-gray-400">
                     All Contracts
                   </div>
