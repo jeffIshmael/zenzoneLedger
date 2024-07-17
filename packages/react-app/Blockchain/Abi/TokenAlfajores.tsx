5887a79f7117074308e1146fe7186564acea779b
jeffIshmael <jeffishmael141@gmail.com>
2024-07-17 16:48:55 +0300

    changing to Celo contract address

--- packages/react-app/Blockchain/Abi/TokenAlfajores.tsx
+++ packages/react-app/Blockchain/Abi/TokenAlfajores.tsx
@@ -1,4 +1,4 @@
-export const tokenAlfajoresContractAddress = "0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1";
+export const tokenAlfajoresContractAddress = "0x765DE816845861e75A25fCA122bb6898B8B1282a";
 export const tokenAlfajoresAbi = [
     {
       constant: true,
